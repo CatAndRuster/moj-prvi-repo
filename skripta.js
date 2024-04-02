cf3b37b7edceac42370ca19bbb3f8498eec10d11
Srpak Goran <srpak.goran7@gmail.com>
2024-04-02 20:57:57 +0200

    Test 2

--- skripta.js
+++ skripta.js
@@ -10,6 +10,7 @@ console.log("Bok!");
 //
 
 console.log("Ovo je prvi update, verzija 2 updejtanja githuba");
+// OVE NAREDBE KO VODU PIT!
 // 1. git add .
 // 2. git commit -m "New console log"  === lokalno updejtamo
 // 3. git push origin master  === pushanje na globalno
