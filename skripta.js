d6fe83697e6fd1de74f68315b04de6408308a1f1
Srpak Goran <srpak.goran7@gmail.com>
2024-04-02 20:51:11 +0200

    last one

--- skripta.js
+++ skripta.js
@@ -10,3 +10,7 @@ console.log("Bok!");
 //
 
 console.log("Ovo je prvi update, verzija 2 updejtanja githuba");
+// 1. git add .
+// 2. git commit -m "New console log"  === lokalno updejtamo
+// 3. git push origin master  === pushanje na globalno
+console.log("bla bla");
