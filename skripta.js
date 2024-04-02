76edf75302454c755fdf430e09e4ba5f8847447e
Srpak Goran <srpak.goran7@gmail.com>
2024-04-02 20:48:58 +0200

    updejtanje

--- skripta.js
+++ skripta.js
@@ -1,1 +1,12 @@
 console.log("Bok!");
+// git init u dobrom folderu da napravimo lokalni repoz.
+
+// git add . - dodavanje svih fajlova
+// git commit - pisanje promjena
+// git commit -m "jasna kratka poruka"
+// git commit -m "git init - inicijalizacija git repozitorija"
+// master vs main
+// glavna grana === master
+//
+
+console.log("Ovo je prvi update, verzija 2 updejtanja githuba");
